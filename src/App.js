fec0a595d9fe427b6f0821b0ad66c863a12da34e
Damian Ziobro <devdamianziobro@gmail.com>
2018-04-23 08:03:17 +0200

    Created counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { hot } from 'react-hot-loader';
 import * as React from 'react';
+
 import Home from './view/Home';
+import Counter from './view/Counter';
 
 import './styles/theme.sass';
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
   render() {
     return (
       <main>
         <Home username="DaftCoder" />
+        <Counter from={170} to={190} />
       </main>
     );
   }
