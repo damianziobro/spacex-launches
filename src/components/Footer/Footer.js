51a48815c4b697739bd52fe7aa5a2ad9eff6a992
Damian Ziobro <devdamianziobro@gmail.com>
2018-05-08 17:33:51 +0200

    Added styles for LaunchDetails footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,29 +5,31 @@ import './Footer.sass';
 function Footer() {
     return (
         <footer className="footer">
-            <h2 className="footer__heading">Follow SpaceX</h2>
-            <ul className="footer__links">
-                <li className="footer__link">
-                    <a href="www.twitter.com" target="_blank">
-                        Twitter
-                    </a>
-                </li>
-                <li className="footer__link">
-                    <a href="www.youtube.com" target="_blank">
-                        Youtube
-                    </a>
-                </li>
-                <li className="footer__link">
-                    <a href="www.flickr.com" target="_blank">
-                        Flickr
-                    </a>
-                </li>
-                <li className="footer__link">
-                    <a href="www.instagram.com" target="_blank">
-                        Instagram
-                    </a>
-                </li>
-            </ul>
+            <div className="links">
+                <h2 className="links__heading">Follow SpaceX</h2>
+                <ul className="links__list">
+                    <li className="links__li">
+                        <a className="links__link" href="www.twitter.com" target="_blank">
+                            Twitter
+                        </a>
+                    </li>
+                    <li className="links__li">
+                        <a className="links__link" href="www.youtube.com" target="_blank">
+                            Youtube
+                        </a>
+                    </li>
+                    <li className="links__li">
+                        <a className="links__link" href="www.flickr.com" target="_blank">
+                            Flickr
+                        </a>
+                    </li>
+                    <li className="links__li">
+                        <a className="links__link" href="www.instagram.com" target="_blank">
+                            Instagram
+                        </a>
+                    </li>
+                </ul>
+            </div>
             <span className="footer__copyright">2018 Space Exploration Technologies Corp.</span>
         </footer>
     );
