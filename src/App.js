7830af2945642413d42074778f0b03c8ba2c342a
Damian Ziobro <devdamianziobro@gmail.com>
2018-05-21 18:02:25 +0200

    Added simple custom routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { hot } from "react-hot-loader";
 import * as React from "react";
 
 import LaunchDetails from "./view/LaunchDetails/LaunchDetails";
+import LaunchesList from "./view/LaunchesList/LaunchesList";
 import Footer from "./components/Footer/Footer";
 
 import "./styles/theme.sass";
@@ -11,16 +12,49 @@ import launchSite from "./assets/sample_json_data/launch_site.json";
 import rocket from "./assets/sample_json_data/rocket.json";
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <main>
+  state = {
+    activeView: "list",
+    launchDetailsId: null
+  };
+
+  handleGoToDetailsClick = (event) => {
+    this.setState({ activeView: "details", launchDetailsId: event.target.id });
+  }
+
+  handleGoToListClick = () => {
+    this.setState({ activeView: "list" });
+  }
+
+  getActiveView = () => {
+    const { activeView } = this.state;
+
+    switch (activeView) {
+      case "list":
+        return (
+          <LaunchesList
+            onGoToDetailsClick={this.handleGoToDetailsClick}
+          />
+        );
+
+      case "details":
+        return (
           <LaunchDetails
             launch={launch}
             launchSite={launchSite}
             rocket={rocket}
+            onGoToListClick={this.handleGoToListBackClick}
           />
-        </main>
+        );
+
+      default:
+        return null;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <main>{this.getActiveView()}</main>
         <Footer />
       </div>
     );
---
+++ src/view/LaunchesList/LaunchesList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LaunchesList extends Component {
+    render() {
+        return (
+            <div className="launches-list">
+            </div>
+        );
+    }
+}
+
+export default LaunchesList;
