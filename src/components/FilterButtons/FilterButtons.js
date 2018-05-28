d2b9cacc3557598dde1ef7adc1019fda8dc24b37
Damian Ziobro <devdamianziobro@gmail.com>
2018-05-29 00:38:30 +0200

    Added styles for filter buttons

--- src/components/FilterButtons/FilterButtons.js
+++ src/components/FilterButtons/FilterButtons.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React from "react"
+
+import "./FilterButtons.sass"
 
 function FilterButtons({ allLaunches, onFilterListClick }) {
-    // Falcon 10 is only for task 5, tesing 'not found'
-  let filters = ['All rockets', "Falcon 10"];
+  // Falcon 10 is only for task 5, tesing 'not found'
+  let filters = ["All rockets", "Falcon 10"]
 
   allLaunches.forEach(launch => {
-    const rocketName = launch.rocket.rocket_name;
+    const rocketName = launch.rocket.rocket_name
     if (filters.indexOf(rocketName) == -1) {
-      filters.push(rocketName);
+      filters.push(rocketName)
     }
-  });
+  })
 
   const filterButtons = filters.map(filter => {
-    const filterName = filter.replace(/\s/g, "").toLowerCase();
+    const filterName = filter.replace(/\s/g, "").toLowerCase()
     return (
-      <button key={filterName} id={filterName} onClick={onFilterListClick}>
+      <button
+        className="filter-buttons__button"
+        key={filterName}
+        id={filterName}
+        onClick={onFilterListClick}
+      >
         {filter}
       </button>
-    );
-  });
+    )
+  })
 
-  return (
-    <div className="filter_buttons">
-      {filterButtons}
-    </div>
-  );
+  return <div className="filter-buttons">{filterButtons}</div>
 }
 
-export default FilterButtons;
+export default FilterButtons
