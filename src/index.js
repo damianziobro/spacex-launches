12fc02231d938080431b96d0b21b1c49573f1e63
Damian Ziobro <devdamianziobro@gmail.com>
2018-07-22 15:18:41 +0200

    Added redux config to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './store/reducers';
 import App from "./App";
 import "./assets/favicon.ico";
 
+const composeEnhancers =
+    process.env.NODE_ENV === 'development'
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        : null || compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk)),
+);
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
