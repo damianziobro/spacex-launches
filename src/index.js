ebada1537b1d0f1bc82917f12a60e0cb0e153af4
Damian Ziobro <devdamianziobro@gmail.com>
2018-07-26 15:49:40 +0200

    Fixed small bug - added compose import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
