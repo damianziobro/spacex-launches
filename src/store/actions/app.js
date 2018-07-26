4e06df709b5748f5b62b968e28a03758c3d53b7d
Damian Ziobro <devdamianziobro@gmail.com>
2018-07-26 18:47:45 +0200

    Deployment

--- src/store/actions/app.js
+++
@@ -1,8 +0,0 @@
-export const STORE_LAUNCH_QUERY_DATA = 'STORE_LAUNCH_QUERY_DATA';
-
-export const storeLaunchQueryData = (flightNumber, rocketName, launchpadName) => ({
-    type: STORE_LAUNCH_QUERY_DATA,
-    flightNumber,
-    rocketName,
-    launchpadName
-});
