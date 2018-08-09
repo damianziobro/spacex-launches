c0c9b5fca002d6e16a3dc61dae222e6daa33b806
Damian Ziobro <devdamianziobro@gmail.com>
2018-08-09 22:20:46 +0200

    Refactored LaunchesList to use separate Launch component

--- src/components/LaunchesList/LaunchesList.js
+++ src/components/LaunchesList/LaunchesList.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { format } from 'date-fns';
 import Footer from '../Footer/Footer';
 
 import logo from '../../assets/img/space_x_logo_bw_centered.png';
-import arrow from '../../assets/img/arrow_pointer.png';
 
 import FilterButtons from '../FilterButtons/FilterButtons';
+import Launch from '../Launch/Launch';
 import Loading from '../UI/Loading/Loading';
 import Error from '../UI/Error/Error';
 
@@ -40,46 +39,7 @@ function LaunchesList({
                 </span>
               ))}
               {(launchesList && (
-                launchesList.map(launch => (
-                  <button
-                    type="button"
-                    className="launch"
-                    key={launch.flight_number}
-                    onClick={event => onGoToDetailsClick(
-                      event,
-                      launch.flight_number,
-                      launch.rocket.rocket_name
-                        .replace(/\s/g, '')
-                        .toLowerCase(),
-                      launch.launch_site.site_id,
-                    )}
-                  >
-                    <span className="launch__date">
-                      {format(launch.launch_date_local, 'DD MMMM YYYY')}
-                    </span>
-                    <div className="launch__arrow">
-                      <img className="launch__arrow-icon" src={arrow} alt="Go to launch details button" />
-                    </div>
-                    <ul className="launch__list">
-                      <li className="launch__list-item">
-                        <div className="launch__name">
-                          Rocket:
-                        </div>
-                        <div className="launch__data">
-                          {launch.rocket.rocket_name}
-                        </div>
-                      </li>
-                      <li className="launch__list-item">
-                        <div className="launch__name">
-                          Launch Site:
-                        </div>
-                        <div className="launch__data">
-                          {launch.launch_site.site_name_long}
-                        </div>
-                      </li>
-                    </ul>
-                  </button>
-                ))
+                launchesList.map(launch => <Launch launch={launch} onGoToDetailsClick={onGoToDetailsClick} />)
               ))}
             </div>
           </div>
